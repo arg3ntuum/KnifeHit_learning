dfbf5b42eaac7ffcaac8bc7c9ef615d42231b084
ROSTYSLAV <45296095+arg3ntuum@users.noreply.github.com>
2022-09-07 15:59:18 +0300

    AddPsevdoRandomChange

--- KnifeHitGit/Assets/Scripts/RandomTargets.cs
+++ KnifeHitGit/Assets/Scripts/RandomTargets.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class RandomTargets : MonoBehaviour
 {
+    [SerializeField] private TargetAndBG[] scriptableObjects;
+    [SerializeField] private GameObject _backGround;
+    [SerializeField] private GameObject _target;
 
 
+    private void Start()
+    {
+        UpdateBGandTarget(Random.Range(0, scriptableObjects.Length));
+    }
+
+    private void UpdateBGandTarget(int _takedSO)
+    {
+        _backGround.GetComponent<SpriteRenderer>().sprite = scriptableObjects[_takedSO].Background;
+        _target.GetComponent<SpriteRenderer>().sprite = scriptableObjects[_takedSO].Target;
+
+    }
 }
